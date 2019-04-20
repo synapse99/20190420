09333a092628e2499cf699a106c19ce0d75497e8
synapse99 <49575952+synapse99@users.noreply.github.com>
2019-04-20 11:21:37 +0900

    Add files via upload

---
+++ loop03.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+print("1부터 9까지")
+for i in range(1, 10, 1):
+    print(i)
+print()
+
+print("0부터 9까지")
+for i in range(10):
+    print(i, end=" ")
+print()
+
+print("0부터 10까지")
+for i in range(11):
+    print(i, end=" ")
+print()
+
+print("1부터 10까지")
+for i in range(1, 11):
+    print(i, end=" ")
+print()
+
+print("1부터 10까지의 홀수")
+for i in range(1, 11, 2):
+    print(i, end=" ")
+print()
+print("2부터 10까지의 짝수")
+for i in range(2, 11, 2):
+    print(i, end=" ")
+print()
---
+++ loop04.py
@@ -0,0 +1,15 @@
+for i in range(1, 101,):
+    s +=i
+
+    if i % 2 = 0:
+        even_s += i
+    else:
+        odd_s +=i
+
+    if i % 7 == 0:
+        sevn_s += i
+        
+print("1부터 100까지의 합 = ",s)
+print("1부터 100까지의 홀수들의 합 = ",odd_s)
+print("1부터 100까지의 짝수들의 합 = ", even_s)
+print("1부터 100까지의 7의 배수들의 합 =",sevn_s) 
